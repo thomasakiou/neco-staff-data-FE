02272910587702878017a2087c63195a928296d5
Coders in Flow <codersinflow@localhost>
2026-01-07 10:10:51 +0100

    uploads

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: '/',
+    baseURL: import.meta.env.VITE_API_URL || 'https://vmi2848672.contaboserver.net/neco-staff/',
 });
 
 api.interceptors.request.use((config) => {
